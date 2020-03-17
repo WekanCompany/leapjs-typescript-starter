8d2f048becf4b70c7329580a363e4cb79f814290
Sanchan Moses <sanchanm@wekancode.com>
2020-03-17 19:40:59 +0000

    feat: wait till queue connections are established

--- src/server.ts
+++ src/server.ts
@@ -8,17 +8,22 @@ import { isValid } from 'configuration/helpers';
 function main(): void {
   configuration
     .init()
-    .then((status: string): any => {
-      Logger.log(`Setting up environment ${status}`, 'ConfigurationManager');
+    .then((): any => {
+      Logger.log(`Setting up environment`, 'ConfigurationManager');
       return isValid(configuration);
     })
-    .then((status: any): any => {
-      Logger.log(`Initializing settings ${status}`, 'ConfigurationManager');
-      Logger.log(`Connecting to the database ${status}`, 'LeapApplication');
-      Logger.log(`Starting server on ${configuration.port}`, 'LeapApplication');
-      bootstrap(configuration);
-      Logger.log(`Up and running`, 'LeapApplication');
-    })
+    .then(
+      async (): Promise<any> => {
+        Logger.log(`Initializing settings`, 'ConfigurationManager');
+        Logger.log(`Connecting to the database`, 'LeapApplication');
+        Logger.log(
+          `Starting server on ${configuration.port}`,
+          'LeapApplication',
+        );
+        await bootstrap(configuration);
+        Logger.log(`Up and running`, 'LeapApplication');
+      },
+    )
     .catch((error): void => {
       Logger.error(expandObject(error), '', 'LeapApplication');
       process.exit(0);
