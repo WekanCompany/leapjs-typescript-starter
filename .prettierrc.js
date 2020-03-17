a790a936a3a165551e709e700b481beaf8c27d75
Sanchan Moses <sanchanm@wekancode.com>
2020-03-17 08:44:41 +0000

    chore: base project configuration

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    semi: true,
+    trailingComma: 'all',
+    singleQuote: true,
+    printWidth: 80,
+    useTabs: false,
+    tabWidth: 2,
+    bracketSpacing: true,
+    bracesSpacing: true,
+    arrowParens: 'always',
+  };
+  
---
+++ README.md
@@ -0,0 +1,9 @@
+# Leap.js typescript starter templates
+
+## Description
+
+Baselines templates build on top of leap.js.
+
+## Getting started
+
+Check out our guide. Visit https://github.com/WekanCompany/leapjs/wiki for more information
