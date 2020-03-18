5767ab93e8bbcd5c76f84b9dc579f9c494f1b116
Sanchan Moses <sanchanm@wekancode.com>
2020-03-18 05:06:06 +0000

    fix(seeder): password is now hashed before a user is seeded

--- tools/seeder/seeders/user.ts
+++ tools/seeder/seeders/user.ts
@@ -2,19 +2,25 @@
 import { RoleModel } from 'app/role/models/role';
 import { Logger } from '@leapjs/common';
 import { UserModel } from 'app/user/models/user';
+import { hash } from 'argon2';
 import roles from '../data/roles';
 import users from '../data/user';
 
 async function UserSeeder(): Promise<boolean> {
   return RoleModel.findOne({ name: roles[0].name })
-    .then((adminRole: any) => {
+    .then(async (adminRole: any) => {
       const defaultEmail = process.env.DEFAULT_ADMIN_EMAIL;
       const defaultPassword = process.env.DEFAULT_ADMIN_PASSWORD;
-      users.forEach((user) => {
-        user.role = adminRole._id;
-        user.email = defaultEmail || '';
-        user.password = defaultPassword || '';
-      });
+      if (defaultPassword && defaultPassword !== '') {
+        const passwordHash = await hash(defaultPassword);
+        users.forEach((user) => {
+          user.role = adminRole._id;
+          user.email = defaultEmail || '';
+          user.password = passwordHash;
+        });
+      } else {
+        throw new Error('Password cannot be empty');
+      }
       return UserModel.insertMany(users);
     })
     .then(() => {
